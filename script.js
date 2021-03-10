4e371a44ca02ba9c0653a5b5e9cda83430c3fdaf
John Stahre <john.kenneth.kauppi@gmail.com>
2021-03-10 20:10:45 +0100

    nåt galet

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const output = document.querySelector('#output');
 let todos = [];
 
 const fetchTodos = () => {
-    fetch('https://jsonplaceholder.typicode.com/todos')
+    fetch('https://jsonplaceholder.typicode.com/todos/?_limit=10')
     .then(res => res.json())
     .then(data => {
         todos = data;
@@ -34,17 +34,17 @@ const newTodo = (todo) => {
 
     let button = document.createElement('button');
     button.classList.add('btn', 'btn-primary');
-    button.innerText = 'X';
+    button.innerText = 'Delete';
     button.addEventListener('click', () => {
         console.log(todo.id)
     })
 
-    let button2 = document.createElement('button');
-    button.classList.add('btn', 'btn-info');
-    button.innerText = 'edit';
-    button.addEventListener('click', () => {
-        console.log('edit')
-    })
+    // let button2 = document.createElement('button');
+    // button.classList.add('btn', 'btn-info');
+    // button.innerText = 'edit';
+    // button.addEventListener('click', () => {
+    //     console.log('edit')
+    // })
 
     //bygg ihop och lägg till i dom
 
@@ -56,7 +56,7 @@ const newTodo = (todo) => {
 }
 
 const createTodo = (title) => {
-    fetch('https://jsonplaceholder.typicode.com/todos/?_limit=10', {
+    fetch('https://jsonplaceholder.typicode.com/todos'), {
         method: 'POST',
         headers: {
             'Content-type': 'application/json; charset=UTF-8'
@@ -72,15 +72,26 @@ const createTodo = (title) => {
     .then(res => res.json())
     .then(data => {
         console.log(data)
+
+        let newTodo = {
+            ...data, //en spread, samma sak som att lägga completed, id och title
+            id: Date.now().toString()
+         }
+        console.log(newTodo);
+        todos.unshift(newTodo);
+        listTodos();
+   
     })
-})
+}
 
 form.addEventListener('submit', e => {
     e.preventDefault();
 
     //ta den lagrade variablen input och skapa en ny to do
 
-    createTodo(input.value)
+    createTodo(input.value);
+    input.value = '';
+    form.requestFullscreen();
 
 })
 
